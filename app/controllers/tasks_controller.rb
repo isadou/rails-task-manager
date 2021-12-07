a1357bc38e0696f17d1272d30e4d4ef5bbf1b222
isabelledouin <isabelle.douin@orange.fr>
2021-12-07 15:08:14 +0100

    table Tasks CRU

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+
+  before_action :set_task, except: [:index, :new, :create]
+  # affiche tous les records de la table
+  def index
+   @tasks = Task.all
+  end
+
+  def show
+    # utilise la methode set_task pour afficher un record avec l'ID indiqué
+  end
+
+  def new
+    @task = Task.new #=> affichage formulaire pour compléter les champs new record
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path #=> redirige vers l'affichage liste taches
+    else
+      render :new
+    end
+  end
+
+  def edit
+    #=> pour afficher toutes les taches la methode set_task et task_params
+  end
+
+  def update
+    @task.update(task_params)
+
+    if @task.save
+      redirect_to tasks_path #=> redirige vers l'affichage liste taches
+    else
+      render :edit
+    end
+  end
+
+  private # ça va nous servir plus tard
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index' #-> list des tasks
+   get  "tasks/new", to: "tasks#new"
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task #=> selectionne un seul record
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task #=> affiche le formulaire et submit
+  patch 'tasks/:id', to: 'tasks#update' #=> modification record existant
 end

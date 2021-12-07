1b876442d720171f250e6dd3f4a862516f4dca36
isabelledouin <isabelle.douin@orange.fr>
2021-12-07 16:56:17 +0100

    table Tasks CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks', to: 'tasks#index' #-> list des tasks
-   get  "tasks/new", to: "tasks#new"
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task #=> selectionne un seul record
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task #=> affiche le formulaire et submit
-  patch 'tasks/:id', to: 'tasks#update' #=> modification record existant
-  delete 'tasks/:id', to: 'tasks#destroy' #=> suppression record existant
+  # get 'tasks', to: 'tasks#index' #-> list des tasks
+  # get  "tasks/new", to: "tasks#new"
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id', to: 'tasks#show', as: :task #=> selectionne un seul record
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task #=> affiche le formulaire et submit
+  # patch 'tasks/:id', to: 'tasks#update' #=> modification record existant
+  # delete 'tasks/:id', to: 'tasks#destroy' #=> suppression record existant
+
+  # la ligne de commande ci-dessous remplace toutes les routes déclarées
+  # ci-dessus et dont les éléments seront récupérées
+  # à partir du controller grâce à rails
+  resources :tasks
 end
